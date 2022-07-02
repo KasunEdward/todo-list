b4b68433678564d512de0ce861279eaec280e3f8
kasunEdward <kasunedward48@gmail.com>
2022-07-03 00:35:27 +0530

    Add more types defs

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,3 +2,11 @@ interface Todo {
     text: string,
     complete: boolean
 }
+
+interface AddTodoObj {
+    todoText: string
+}
+
+type ToggleTodo = (selectedTodo: Todo) => void;
+
+type AddTodo = (todoObj: AddTodoObj) => void;
